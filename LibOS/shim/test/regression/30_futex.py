75636f44e7ce4df7716311d9f3e64a7fc28d8e2a
Yupeng Liao <you@example.com>
2019-04-03 01:06:14 -0700

    initial checkin

---
+++ LibOS/shim/test/regression/30_futex.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+
+import os, sys, mmap
+from regression import Regression
+
+loader = sys.argv[1]
+
+# Running futex
+regression = Regression(loader, "futex")
+
+regression.add_check(name="Futex Wake Test",
+    check=lambda res: "Woke all kiddos" in res[0].out)
+
+rv = regression.run_checks()
+if rv: sys.exit(rv)
---
+++ Pal/regression/Exception.c
@@ -0,0 +1,61 @@
+/* -*- mode:c; c-file-style:"k&r"; c-basic-offset: 4; tab-width:4; indent-tabs-mode:nil; mode:auto-fill; fill-column:78; -*- */
+/* vim: set ts=4 sw=4 et tw=78 fo=cqt wm=0: */
+
+/* This Hello World simply print out "Hello World" */
+
+#include "pal.h"
+#include "pal_debug.h"
+
+void handler1 (PAL_PTR event, PAL_NUM arg, PAL_CONTEXT * context)
+{
+    pal_printf("Arithmetic Exception Handler 1: %p, rip = %p\n",
+               arg, context->rip);
+
+    while (*(unsigned char *) context->rip != 0x90)
+        context->rip++;
+
+    DkExceptionReturn(event);
+}
+
+void handler2 (PAL_PTR event, PAL_NUM arg, PAL_CONTEXT * context)
+{
+    pal_printf("Arithmetic Exception Handler 2: %p, rip = %p\n",
+               arg, context->rip);
+
+    while (*(unsigned char *) context->rip != 0x90)
+        context->rip++;
+
+    DkExceptionReturn(event);
+}
+
+void handler3 (PAL_PTR event, PAL_NUM arg, PAL_CONTEXT * context)
+{
+    pal_printf("Memory Fault Exception Handler: %p, rip = %p\n",
+               arg, context->rip);
+
+    while (*(unsigned char *) context->rip != 0x90)
+        context->rip++;
+
+    DkExceptionReturn(event);
+}
+
+int main (void)
+{
+    volatile long i;
+
+    DkSetExceptionHandler(handler1, PAL_EVENT_ARITHMETIC_ERROR, 0);
+    i = 0;
+    i = 1 / i;
+    __asm__ volatile("nop");
+
+    DkSetExceptionHandler(handler2, PAL_EVENT_ARITHMETIC_ERROR, 0);
+    i = 0;
+    i = 1 / i;
+    __asm__ volatile("nop");
+
+    DkSetExceptionHandler(handler3, PAL_EVENT_MEMFAULT, 0);
+    *(volatile long *) 0x1000 = 0;
+    __asm__ volatile("nop");
+
+    return 0;
+}
